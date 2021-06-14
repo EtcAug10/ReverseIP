933b14b4a26a37dba3d41a7ed50b0c3de14f8310
Arya Kresna <aryakrishna436@gmail.com>
2021-06-14 18:54:13 +0700

    Update reverseip.py

--- reverseip.py
+++ reverseip.py
@@ -1,11 +1,12 @@
 from requests import Session,post,get
 from re import match,findall
+import json
 
-__version__ = "1.0.0"
+__version__ = "1.0.7"
 __name__ = "Reverse Ip Library"
 __author__ = "Arya Kresna"
 
-class Osint_SH:
+class OsintSH:
   
   def __init__(self):
     self.url = "https://osint.sh/reverseip/"
@@ -24,3 +25,19 @@ class Osint_SH:
     else:
       r = post(self.url,data=self.data)
     return findall(r"Domain\">(.*?)<\/",r.text.replace("\n","").replace(" ",""))
+
+class SonarOmnisintIO:
+  
+  def __init__(self):
+    self.url = "https://sonar.omnisint.io/reverse/"
+    self.ip = ""
+    self.headers = ""
+  
+  def get(self,ip,headers=""):
+    self.ip = ip
+    self.headers = headers
+    return True
+    
+  def dump(self):
+    req = get(self.url+self.ip,headers=self.headers).text
+    return json.loads(req)
